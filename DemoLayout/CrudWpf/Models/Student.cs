40fb3990f5fbb5375710ad6f261783b65eaab8fb
tungnm <kakal0tdz04@gmail.com>
2024-09-23 18:13:52 +0700

    binding gender

--- DemoLayout/CrudWpf/Models/Student.cs
+++ DemoLayout/CrudWpf/Models/Student.cs
@@ -9,7 +9,7 @@ namespace CrudWpf.Models
         public string Name { get; set; } = null!;
         public bool Gender { get; set; }
 
-        //public bool Female { get => |Gender; }
+        public bool Female { get => !Gender; }
         public string DepartId { get; set; } = null!;
         public DateTime? Dob { get; set; }
         public double Gpa { get; set; }
